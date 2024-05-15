609d90bc067371d1ebf1b79f9a21ac417e794a80
PE�ACUBA RHYLL KYLE BIBAOCO <2020476@cc.usls.edu>
2024-05-15 15:40:54 +0800

    change

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,38 @@
 
-public class Main {
+import javax.swing.*;
+import java.awt.*;
+
+public class Main extends JFrame {
+    public Main() { init(); }
+
+    public void init() {
+
+        JPanel mypanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+        JButton okbtn = new JButton("OK");
+        JButton cancelbtn = new JButton("Cancel");
+
+        Container container = this.getContentPane();
+        StudentsPanel studentsPanel = new StudentsPanel();
+        CoursePanel coursePanel = new CoursePanel();
+
+
+        mypanel.add(okbtn);
+        mypanel.add(cancelbtn);
+
+
+        container.setLayout(new BorderLayout());
+        container.add(studentsPanel, BorderLayout.NORTH);
+        container.add(coursePanel, BorderLayout.CENTER);
+        container.add(mypanel, BorderLayout.SOUTH);
+
+
+        this.setResizable(false);
+        this.pack();
+        this.setTitle(" ");
+        this.setVisible(true);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
     public static void main(String[] args) {
-        new MainFrame();
+        new Main();
     }
 }
