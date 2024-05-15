700b251cb21c4d1641313ea1f099344738cdeeed
HINAYAN JULIET BEATRICE LUMAWAG <1921061@cc.usls.edu>
2024-05-15 16:19:25 +0800

    made changes

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,30 @@
+import javax.swing.*;
+import java.awt.*;
 
-public class Main {
+public class Main extends JFrame {
+    public Main() { init(); }
+
+    public void init() {
+
+        JPanel panel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+
+
+
+        Container container = this.getContentPane();
+        CoursePanel CoursePanel = new CoursePanel();
+
+
+
+        container.setLayout(new BorderLayout());
+        container.add(CoursePanel, BorderLayout.NORTH);
+        container.add(panel, BorderLayout.SOUTH);
+
+        this.setVisible(true);
+        this.pack();
+        this.setTitle("HINAYAN");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
     public static void main(String[] args) {
-        new MainFrame();
+        new Main();
     }
 }
