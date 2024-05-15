79cfd00461079a3ca0c24aa32fb6073b818827bf
poljan <misoopotamia@gmail.com>
2024-05-15 17:06:10 +0800

    update

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,5 @@
-import javax.swing.*;
-import java.awt.*;
-public class Main extends JFrame {
-    public Main() { init(); }
-    public void init() {
-        JPanel panel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
-
-        Container container = this.getContentPane();
-        StudentsPanel namepanel = new StudentsPanel();
-
-        container.setLayout(new BorderLayout());
-        container.add(namepanel, BorderLayout.NORTH);
-        container.add(panel, BorderLayout.SOUTH);
-
-        this.setVisible(true);
-        this.pack();
-        this.setTitle("Joaquin ContactEditorUI");
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    }
+public class Main {
     public static void main(String[] args) {
-        new Main();
+        new MainFrame();
     }
 }
