5e9f7a1ebc24e8acb533e38bbcbd83021beb0fae
Cheesecakeyyy <104256610+Cheesecakeyyy@users.noreply.github.com>
2024-05-22 13:32:11 +0800

    main updated

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,19 @@
 import javax.swing.*;
-import java.awt.*;
 
-public class Main extends JFrame {
-    public Main() { init(); }
-
-    public void init() {
-
-        JPanel mypanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
-        JButton okbtn = new JButton("OK");
-        JButton cancelbtn = new JButton("Cancel");
-
-
-        Container container = this.getContentPane();
-        StudentsPanel studentsPanel = new StudentsPanel();
-        CoursePanel coursePanel = new CoursePanel();
-
-
-        mypanel.add(okbtn);
-        mypanel.add(cancelbtn);
-        
-
-
-        container.setLayout(new BorderLayout());
-        container.add(studentsPanel, BorderLayout.NORTH);
-        container.add(coursePanel, BorderLayout.CENTER);
-        container.add(mypanel, BorderLayout.SOUTH);
-
-
-        this.setResizable(false);
-        this.pack();
-        this.setTitle("RDH");
-        this.setVisible(true);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    }
+public class Main {
     public static void main(String[] args) {
-        new Main();
+
+        JFrame studentsFrame = new JFrame();
+        studentsFrame.add(new StudentsPanel());
+        studentsFrame.pack();
+        studentsFrame.setVisible(true);
+
+
+        //display course frame
+        JFrame courseFrame = new JFrame();
+        courseFrame.add(new CoursePanel());
+        courseFrame.pack();
+        courseFrame.setVisible(true);
+
     }
 }
